4a9a054bb234360bc32390f49e9fcbdd667fc1c6
FabianoCarregas <fabianofac_@hotmail.com>
2022-02-19 10:34:46 +0100

    ultimo

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -6,18 +6,23 @@
 /*   By: faaugust <faaugust@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 12:53:49 by faaugust          #+#    #+#             */
-/*   Updated: 2022/02/18 00:37:44 by faaugust         ###   ########.fr       */
+/*   Updated: 2022/02/18 23:18:28 by faaugust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
 	i = 0;
-	while ((s1[i] == s2[i]) && (s1[i] != '\0') && (s2[i] != '\0') && (i < n))
+	while ((s1[i] == s2[i]) && (s1[i] != '\0') && (0 < n))
 	{
 		i++;
+		n--;
 	}
+	if (n == 0)
+		return (0);
 	return (s1[i] - s2[i]);
 }
